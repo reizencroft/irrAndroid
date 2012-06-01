e0daa2342fabf6daad9abbd2e8bbf1e0d8688ad4
reizencroft <reizencroft@gmail.com>
2012-06-01 21:44:16 +0800

    example 2 now compiles

---
+++ examples/02.Quake3Map/jni/Android.mk
@@ -0,0 +1,3 @@
+JNI_FOLDER := $(call my-dir)
+include $(JNI_FOLDER)/../../../source/Irrlicht/Android.mk
+include $(JNI_FOLDER)/../Android.mk
---
+++ examples/02.Quake3Map/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_MODULES := IrrAndroid Quake3Map
+APP_PLATFORM :=android-9
